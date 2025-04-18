345d466b3b4b78dcc67aa5b28c38f4fedcbc0bbd
Carlos Damota <cdamota.cd@gmail.com>
2025-04-19 00:01:03 +0200

    correction model project

--- models/projectModel.js
+++ models/projectModel.js
@@ -1,29 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const projectSchema = new mongoose.Schema({
-    
-    projects:[
-        {
-            name: { type: String },
-            description: { type: String },
-            urls: { type: Array },
-            technologiesUsed: [{ type: String }],
-            duration: { type: String },
-            type: [{ type: String }],
-            date: { type: Date },
-            // Mentions? => for later
-            multimedia: { type: String }
-        }
-    ],
+const projectSchema = new mongoose.Schema(
+  {
+    name: { type: String },
+    description: { type: String },
+    urls: { type: Array },
+    technologiesUsed: [{ type: String }],
+    duration: { type: String },
+    type: [{ type: String }],
+    date: { type: Date },
+    // Mentions? => for later
+    multimedia: { type: String },
     owner: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'User',                          
-        required: false                       
-      },
-    
-}, {
-  timestamps: true
-});
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Project = mongoose.model('Project', projectSchema);
+const Project = mongoose.model("Project", projectSchema);
 module.exports = Project;
