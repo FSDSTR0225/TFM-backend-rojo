511ab63d88ac66de191137b2fee94238fcb8e996
Jeremie <jeremiechiclote34@gmail.com>
2025-04-20 15:26:05 +0200

    actualizacion del perfil Recruiter

--- controllers/recruiterController.js
+++ controllers/recruiterController.js
@@ -19,6 +19,20 @@ module.exports = {
         } catch (error) {
             res.status(500).json({ msg: error.message});
         }
+    },
+
+    updateRecruiterProfile:async(req,res)=>{
+        try{
+            const datosRecruiter = req.body;
+            console.log('Datos obtenidos del Front: ', datosRecruiter);
+            if(!datosRecruiter) return res.status(400).json({ msg: 'Some required fields are missing' });
+            const recruiter = await Recruiter.findByIdAndUpdate(req.params.id,
+                {"roles.recruiter":datosRecruiter},
+                {new:true});
+            res.status(200).json({msg:'Recruiter updated', recruiter});
+        }catch(error){
+            res.status(500).json({ msg: error.message});
+        }
     }
 }
 
