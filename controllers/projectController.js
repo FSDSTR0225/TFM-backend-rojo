f8e80075bb60068f16e7d29bad61699f1cd924e5
Beliochando <beliochando@gmail.com>
2025-04-27 18:39:54 +0200

    codigos project

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -20,25 +20,46 @@ module.exports = {
         }
     },
 
-    createProject:async(req, res)=>{
+    createProject: async (req, res) => {
         try {
-            const project = req.body;
-            console.log('Valor del frontEnd: ',project);
+            const userId = req.user.id; // Recoge el id del usuario
+            const project = { ...req.body, owner: userId }; //Recoge los datos del usuario y le añade el id del dueño de la tarea.
+            console.log('Frontend value: ', project);
             const newProject = new Project(project);
             await newProject.save();
-            res.json({project: newProject, msg: 'Proyecto creado correctamente'});
+            res.json({ project: newProject, msg: 'Project created successfully' });
         } catch (error) {
-            res.status(500).json({ msg: error.message});
+            res.status(500).json({ msg: error.message });
         }
     },
 
-    updateProject:async(req,res)=>{
+    updateProject: async (req, res) => {
         try {
+            const projectId = req.params.id; //Recoge el id del proyecto
+            const userId = req.user.id; // Recoge el id del usuario
+            const projectNewData = req.body; // Recoge los datos del body para pasar al backend
+    
+            const project = await Project.findById(projectId); // Encuentra la tarea por el id de la tarea
+            if (!project) {
+                return res.status(404).json({ msg: 'Project not found' });
+            }
+    
+            if (project.owner.toString() !== userId) { //Comprueba que corresponda con el id del usuario y, si no es el mismo, no puede actualizar el proyecto
+                return res.status(403).json({ msg: 'You are not authorized to update this project' });
+            }
+    
+            const updatedProject = await Project.findByIdAndUpdate( //actualiza el proyecto
+                projectId, // toma el id del proyecto
+                { ...projectNewData }, // le mete los nuevos datos
+                { new: true }
+            );
+    
+            return res.status(200).json({ msg: 'Project updated successfully', updatedProject }); //devuelve los datos actualizados
             
         } catch (error) {
-            res.status(500).json({ msg: error.message});
+            return res.status(500).json({ msg: error.message });
         }
     }
 
-}
-
+}   
+    
--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -4,5 +4,7 @@ const projectController = require('../controllers/projectController');
 
 router.get('/', projectController.getProjects);
 router.get('/:id', projectController.getProjectById);
+router.post('/', isAuthenticated, projectController.createProject);
+router.put('/:id', isAuthenticated, projectController.updateProject);
 
 module.exports = router;
