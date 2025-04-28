77011571dfd79c8f493d5b26b5671cef591c2b6d
Beliochando <beliochando@gmail.com>
2025-04-28 10:40:13 +0200

    code resoluted

--- controllers/devController.js
+++ controllers/devController.js
@@ -3,156 +3,79 @@ const User = require('../models/userModel'); // Asegúrate de que la ruta sea co
 
 
 module.exports = {
-<<<<<<< HEAD
-  //Desestructurar para no mostrar contraseña
-      getDevs : async (req,res) => {
-          try {
-              const devs = await User.find({"roles.type": "developer"});
-              res.json(devs);
-          } catch (error) {
-              res.status(500).json({ msg: error.message});
-          }
-      },
-  
-      //Este código nos muestra el perfil de cada dev
-      getDevById : async (req,res) => {
-          try {
-              const dev = await User.findById(req.params.id);
-              res.json(dev);
-          } catch (error) {
-              res.status(500).json({ msg: error.message});
-          }
-      },
-  
-      updateDevProfile: async (req, res) => {
-        try {
-          const {
-            _id,
-            professionalPosition,
-            location,
-            instagram,
-            linkedin,
-            github,
-            skills,
-            languages,
-            description,
-          } = req.body;
-      
-          if (
-            !_id || !professionalPosition || !location || !skills || !languages 
-          ) {
-            return res.status(400).json({ msg: 'Some required fields are missing' });
-          }
-      
-          const user = await User.findById(_id);
-          if (!user) {
-            return res.status(404).json({ msg: 'User not found' });
-          }
-      
-          if (!user.roles || !user.roles.type) {
-            return res.status(400).json({ msg: 'User role type is missing' });
-          }
-      
-          if (user.roles.type !== 'developer') {
-            return res.status(400).json({ msg: 'User must have the role of "developer"' });
-          }
-  
-        const updatedUser= {...user, 
-          description,
-          roles: {
-            developer: {
-            professionalPosition,
-            location,
-            instagram,
-            linkedin,
-            github,
-            skills,
-            languages,
-            }
-          }
-        }
-          await user.save();
-      
-          res.status(201).json(user);
-        } catch (error) {
-          res.status(500).json({ msg: error.message });
-        }
-      }
-=======
-//Desestructurar para no mostrar contraseña
-    getDevs : async (req,res) => {
-        try {
-            const devs = await User.find({"roles.type": "developer"});
-            res.json(devs);
-        } catch (error) {
-            res.status(500).json({ msg: error.message});
-        }
-    },
-
-    //Este código nos muestra el perfil de cada dev
-    getDevById : async (req,res) => {
-        try {
-            const dev = await User.findById(req.params.id);
-            res.json(dev);
-        } catch (error) {
-            res.status(500).json({ msg: error.message});
-        }
-    },
-
-    updateDevProfile: async (req, res) => {
-      try {
-        const {
-          _id,
-          professionalPosition,
-          location,
-          instagram,
-          linkedin,
-          github,
-          skills,
-          languages,
-          description,
-        } = req.body;
-    
-        if (
-          !_id || !professionalPosition || !location || !skills || !languages 
-        ) {
-          return res.status(400).json({ msg: 'Some required fields are missing' });
-        }
-    
-        const user = await User.findById(_id);
-        if (!user) {
-          return res.status(404).json({ msg: 'User not found' });
-        }
-    
-        if (!user.roles || !user.roles.type) {
-          return res.status(400).json({ msg: 'User role type is missing' });
-        }
-    
-        if (user.roles.type !== 'developer') {
-          return res.status(400).json({ msg: 'User must have the role of "developer"' });
-        }
-
-      const updatedUser= {...user, 
-        description,
-        roles: {
-          developer: {
-          professionalPosition,
-          location,
-          instagram,
-          linkedin,
-          github,
-          skills,
-          languages,
-          }
-        }
-      }
-      await User.findByIdAndUpdate(_id, updatedUser, { new: true });
-
-
-      res.status(200).json({ msg: 'Developer profile updated successfully', user: updatedUser });
+  getDevs : async (req,res) => {
+    try {
+        const devs = await User.find({"roles.type": "developer"});
+        res.json(devs);
     } catch (error) {
-      res.status(500).json({ msg: error.message });
+        res.status(500).json({ msg: error.message});
     }
+},
+
+//Este código nos muestra el perfil de cada dev
+getDevById : async (req,res) => {
+    try {
+        const dev = await User.findById(req.params.id);
+        res.json(dev);
+    } catch (error) {
+        res.status(500).json({ msg: error.message});
+    }
+},
+
+updateDevProfile: async (req, res) => {
+  try {
+    const {
+      _id,
+      professionalPosition,
+      location,
+      instagram,
+      linkedin,
+      github,
+      skills,
+      languages,
+      description,
+    } = req.body;
+
+    if (
+      !_id || !professionalPosition || !location || !skills || !languages 
+    ) {
+      return res.status(400).json({ msg: 'Some required fields are missing' });
+    }
+
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).json({ msg: 'User not found' });
+    }
+
+    if (!user.roles || !user.roles.type) {
+      return res.status(400).json({ msg: 'User role type is missing' });
+    }
+
+    if (user.roles.type !== 'developer') {
+      return res.status(400).json({ msg: 'User must have the role of "developer"' });
+    }
+
+  const updatedUser= {...user, 
+    description,
+    roles: {
+      developer: {
+      professionalPosition,
+      location,
+      instagram,
+      linkedin,
+      github,
+      skills,
+      languages,
+      }
+    }
+  }
+  await User.findByIdAndUpdate(_id, updatedUser, { new: true });
+
+
+  res.status(200).json({ msg: 'Developer profile updated successfully', user: updatedUser });
+} catch (error) {
+  res.status(500).json({ msg: error.message });
+}
 },
 
 }
