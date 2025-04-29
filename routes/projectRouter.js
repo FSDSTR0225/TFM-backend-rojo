7f27d3b6518d4e707ffb755e127aba97592201a5
Carlos Damota <cdamota.cd@gmail.com>
2025-04-29 21:37:11 +0200

    fix: error

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
+const isAuthenticated = require('../middlewares/auth/isAutenticated');
 
 router.get('/', projectController.getProjects);
 router.get('/:id', projectController.getProjectById);
