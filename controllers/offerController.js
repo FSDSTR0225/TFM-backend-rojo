62799731c69b743002d71fab2b13bfce52da25d0
Carlos Damota <cdamota.cd@gmail.com>
2025-05-05 22:32:08 +0200

    feat: update getOfferById to include detailed owner information in response

--- controllers/offerController.js
+++ controllers/offerController.js
@@ -17,7 +17,7 @@ module.exports = {
             //Filtrado por no borradas
             const offer = await Offer.findOne({_id: req.params.id , isDelete:false}).populate({
                 path: 'owner',
-                select: 'name, logo, companyName, website, contact '
+                select: 'name role.type role.recruiter.logo role.recruiter.companyName role.recruiter.website role.recruiter.contact'
             });
             if (!offer) {
                 return res.status(404).json({ msg: 'Offer not found' });
