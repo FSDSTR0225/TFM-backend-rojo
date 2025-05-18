2f56ad0f36f3e80a20c5e3c81487431a33ddf773
Beliochando <beliochando@gmail.com>
2025-05-18 09:06:30 +0200

    changes projecmodel

--- models/projectModel.js
+++ models/projectModel.js
@@ -2,29 +2,25 @@ const mongoose = require("mongoose");
 
 const projectSchema = new mongoose.Schema(
   {
-    title: { type: String },
+    title: { type: String, required: true },
     description: { type: String },
     category: { type: String },
-    technologiesUsed: [{ type: String }],
+    projectSkills: [{ type: String }], // o technologiesUsed si prefieres ese nombre 
+    professionalRole: { type: String },
     duration: { type: String },
-    types: [{ type: String }],
-    date: { type: Date },
-    image: { type: String },
-    urls: [{ type: String }],
-    multimedia: { type: String },
+    year: { type: Number },
+    gallery: [{ type: String }], // URLs de imágenes
+    videoUrl: { type: String, default: null },
+    codeSections: [{ type: String }],
+    liveLink: { type: String },
+    githubProjectLink: { type: String },
     owner: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: false,
     },
-    isDeleted: {
-      type: Boolean,
-      default: false,
-    },
-    deletedAt: {
-      type: Date,
-      default: null,
-    },
+    isDeleted: { type: Boolean, default: false },
+    deletedAt: { type: Date, default: null },
   },
   {
     timestamps: true,
