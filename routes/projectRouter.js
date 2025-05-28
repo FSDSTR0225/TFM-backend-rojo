35119483be9af2d05790cb0e99fbde1f837afcb2
antonioj-arce <antonioj.arce@hotmail.com>
2025-05-28 17:14:02 +0200

    changes in getProjectsByDeveloper

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -5,6 +5,7 @@ const isAuthenticated = require('../middlewares/auth/isAutenticated');
 
 router.get('/', projectController.getProjects);
 router.get('/:id', projectController.getProjectById);
+router.get('/developer/:developerId', projectController.getProjectsByDeveloper);
 router.post('/', isAuthenticated, projectController.createProject);
 router.put('/:id', isAuthenticated, projectController.updateProject);
 router.delete("/:id", isAuthenticated, projectController.deleteProject);
