8a391e19aba7a71abfda7719781400d6dd9f901a
Beliochando <beliochando@gmail.com>
2025-06-07 06:25:51 +0200

    like and views model and controllers implemented

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -9,5 +9,7 @@ router.get('/developer/:developerId', projectController.getProjectsByDeveloper);
 router.post('/', isAuthenticated, projectController.createProject);
 router.put('/:id', isAuthenticated, projectController.updateProject);
 router.delete("/:id", isAuthenticated, projectController.deleteProject);
+router.post('/:id/view', projectController.incrementView);
+router.post('/:id/like', isAuthenticated, projectController.toggleLike);
 
 module.exports = router;
