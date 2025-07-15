bd8f5d7c85a4d3be2ef90fece9a63d5844b6c607
Jeremie <jeremiechiclote34@gmail.com>
2025-07-15 13:49:57 +0200

    Eliminacion del bodyParser

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const uploadRouter = require('./routes/uploadRouter');
 //Configuraciones del servidor 
 const { app,server }= require("./utils/socket"); // Importa la instancia de Express desde socket.js
 const port = process.env.PORT;
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cors = require('cors');
 // const server = app.listen(port, () => {
 //   console.log(`Server running on port ${port}`);
@@ -73,10 +73,8 @@ app.use(cors({
 
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
